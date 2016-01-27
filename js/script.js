960459f08658db037f872e19bde18981dda4eb22
Nick Birch <nicholas.birch2@gmail.com>
2016-01-26 20:20:26 -0800

    fixed grid sizing changing function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-//var size = 40;
-
 $(document).ready(function(){
 	//create grid of 16x16 scquares
 	for(var x = 0; x<16; x++){
 		for(var y = 0; y<16; y++){
-			var square = $("<div class = 'square'></div>");
-			square.appendTo("#container");
+			$("#container").append("<div class = 'square'></div>");
 		}
+		$('#container').append("<div class='new_row'></div>");
 	}
 	//change background colour of squares when mouse hovers over
 	$(".square").hover(function(){
@@ -16,29 +14,21 @@ $(document).ready(function(){
 
 function reset(){
 	//prompt user for grid size
-	do{size = parseInt(prompt("Please enter from 1 to 40: "));
-	}while(isNaN(size) || size<1 || size >40);
-	//remove all existing grid squares
+	do{size = parseInt(prompt("Please enter from 1 to 64: "));
+	}while(isNaN(size) || size<1 || size >64);
+	var squareSize = $("#container").width()/size - 2;
 	$(".square").remove();
-	//set width and height of grid in squares
-	if(704%(size+4) != 0){
-		var width = Math.floor(704/(size+4));
-		var height = Math.floor(704/(size+4));
-	}else{
-		width = 704/(size+4);
-		height = 704/(size+4);
-	}
 	//populate grid
-	for(var x = 0; x<width; x++){
-		for(var y = 0; y<height; y++){
-			var square = $("<div class = 'square'></div>");
-			square.appendTo("#container");
+	for(var x = 0; x<size; x++){
+		for(var y = 0; y<size; y++){
+			$("#container").append("<div class = 'square'></div>");
 		}
+		$("#container").append("<div class='new_row'></div>");
 	}
 	//change square size
-	$(".square").css({
-		"width": size+"px", 
-		"height": size+"px"});
+	$('.square').css('width',squareSize);
+	$('.square').css('height',squareSize);
+	//change background colour of squares when mouse hovers over
 	$(".square").hover(function(){
 		$(this).addClass("hovered");
 	});
